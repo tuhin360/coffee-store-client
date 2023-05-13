d2ac5e59fdfdc41dd9cc7990e371f0450553195a
tuhin <tuhin153695@gmail.com>
2023-05-14 04:00:49 +0600

    coffee crud form added

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,9 +1,50 @@
+import Swal from 'sweetalert2';
+
 const AddCoffee = () => {
+  
+  const handleAddCoffee = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const quantity = form.quantity.value;
+    const supplier = form.supplier.value;
+    const taste = form.taste.value;
+    const category = form.category.value;
+    const details = form.details.value;
+    const photo = form.photo.value;
+
+    const newCoffee ={name, quantity, supplier, taste, category, details, photo};
+    console.log(newCoffee);
+
+    // send data to the server
+    fetch('http://localhost:5000/coffee', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(newCoffee)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      if (data.insertedId){
+        Swal.fire({
+          title: 'Success!',
+          text: 'Coffee Added Successfully',
+          icon: 'success',
+          confirmButtonText: 'Cool'
+        })
+      }
+    })
+  }
+
+
+
+
   return (
     <div className="bg-[#F4F3F0] p-24">
       <h2 className="text-3xl font-extrabold">Add a Coffee</h2>
-
-      <form>
+      <form onSubmit={handleAddCoffee}>
          {/* form name & quantity row  */}
         <div className="md:flex mb-8">
           <div className="form-control md:w-1/2">
@@ -19,7 +60,6 @@ const AddCoffee = () => {
               />
             </label>
           </div>
-
           <div className="form-control md:w-1/2 ml-4">
             <label className="label">
               <span className="label-text">Available Quantity</span>
@@ -50,7 +90,6 @@ const AddCoffee = () => {
               />
             </label>
           </div>
-
           <div className="form-control md:w-1/2  ml-4">
             <label className="label">
               <span className="label-text">Taste</span>
@@ -81,7 +120,6 @@ const AddCoffee = () => {
               />
             </label>
           </div>
-
           <div className="form-control md:w-1/2  ml-4">
             <label className="label">
               <span className="label-text">Details</span>
@@ -106,7 +144,7 @@ const AddCoffee = () => {
             <label className="input-group">
               <input
                 type="text"
-                name="details"
+                name="photo"
                 placeholder="Photo URL"
                 className="input input-bordered w-full bg-white"
               />
